57125a0320df2aafcab5ca6fd502326c365ecfe3
Sittisukintaruk <fah78963214@gmail.com>
2022-07-03 17:18:44 +0700

    submit]

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./**.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': 'hsl(0, 0%, 100%)',
+      'Soft-blue': 'hsl(215, 51%, 70%)',
+      'Cyan': ' hsl(178, 100%, 50%)',
+      'Very-dark-blue-main-bg': 'hsl(217, 54%, 11%)',
+      'Very-dark-blue-card-bg': 'hsl(216, 50%, 16%)',
+      'Very-dark-blue-line': 'hsl(215, 32%, 27%)',
+    },
+    fontFamily:{
+      'sans': ['Outfit', 'sans-serif']
+    },
+    
+  },
+  plugins: [],
+};
